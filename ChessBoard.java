5f3b8084bda72b11c7a1f11d397a79a33e799c1a
Aaron Graham <Aaron@Aaron-Grahams-iMac.local>
2020-04-21 19:39:52 -0400

    Added support for descriptive move notation

--- ChessBoard.java
+++ ChessBoard.java
@@ -5,6 +5,7 @@ public class ChessBoard {
     static final int chess_board_size = 8;
 
     private final char[][] chess_board;
+    public boolean descriptive_moves;
     public Stack<Command> undoLog;
 
     public ChessBoard() {
@@ -17,15 +18,30 @@ public class ChessBoard {
                                           {'p','p','p','p','p','p','p','p'},
                                           {'c','h','b','q','k','b','h','c'}};
 
+        this.descriptive_moves = false;
         this.undoLog = new Stack<>();
     }
 
     public void print() {
-      System.out.println("     C  C  C  C  C  C  C  C  ");
-      System.out.println("     1  2  3  4  5  6  7  8  ");
-      System.out.println("    ------------------------ ");
+        if (this.descriptive_moves == false)
+        {
+            System.out.println("     C  C  C  C  C  C  C  C  ");
+            System.out.println("     1  2  3  4  5  6  7  8  ");
+            System.out.println("    ------------------------ ");
+        }
+        else
+        {
+            System.out.println(" ------------------------ ");
+        }
         for(int i=0; i<chess_board_size; ++i) {
-            System.out.print("R" + Integer.toString(i+1) + " |");
+            if (this.descriptive_moves == false)
+            {
+                System.out.print("R" + Integer.toString(i+1) + " |");
+            }
+            else
+            {
+                System.out.print("|");
+            }
             for (int j=0; j<chess_board_size; ++j) {
                 if ((i % 2) == (j % 2))
                 {
@@ -38,7 +54,14 @@ public class ChessBoard {
             }
             System.out.println("|");
         }
-        System.out.println("     ------------------------ ");
+        if (this.descriptive_moves == false)
+        {
+            System.out.println("     ------------------------ ");
+        }
+        else
+        {
+            System.out.println(" ------------------------ ");
+        }
     }
 
     public char getTilePiece(ChessTile chessTile)
@@ -51,52 +74,6 @@ public class ChessBoard {
         chess_board[chessTile.getRow()][chessTile.getCol()] = piece;
     }
 
-    public char movePiece(int old_row, int old_col, int new_row, int new_col)
-    {
-        if (old_row < 1 || old_row > chess_board_size || old_col < 1 || old_col > chess_board_size) {
-            System.out.println("Not a valid location" );
-            return ' ';
-        }
-        placePiece(new_row, new_col, chess_board[old_row-1][old_col-1]);
-        placePiece(old_row, old_col, ' ');
-        return chess_board[old_row-1][old_col-1]; 
-    }
-
-    public char placePiece(int row, int col, char piece) {
-        if (row < 1 || row > chess_board_size || col < 1 || col > chess_board_size) {
-            System.out.println("Not a valid location" );
-            return ' ';
-        }
-        char tmp = chess_board[row-1][col-1];
-        chess_board[row-1][col-1] = piece;
-        return tmp;
-    }
-
-    public void undoMove(String move) {
-        String[] command_tokens = move.split(" ");
-        switch(command_tokens[0]) {
-            case "move":
-                int old_row = Integer.parseInt(command_tokens[1]);
-                int old_col = Integer.parseInt(command_tokens[2]);
-                int new_row = Integer.parseInt(command_tokens[3]);
-                int new_col = Integer.parseInt(command_tokens[4]);
-                char removed_piece = command_tokens[5].charAt(0);
-                removed_piece = (removed_piece == '_') ? ' ' : removed_piece;
-                char curr_piece = chess_board[new_row-1][new_col-1];
-                placePiece(new_row,new_col,removed_piece);
-                placePiece(old_row,old_col,curr_piece);
-                break;
-            case "place":
-                char unneeded_piece = command_tokens[1].charAt(0);
-                int row = Integer.parseInt(command_tokens[2]);
-                int col = Integer.parseInt(command_tokens[3]);
-                removed_piece = command_tokens[4].charAt(0);
-                removed_piece = (removed_piece == '_') ? ' ' : removed_piece;
-                placePiece(row, col, removed_piece);
-                break;
-        }
-    }
-
     public static void main(String[] args) {
         ChessBoard game = new ChessBoard();
         Scanner input = new Scanner(System.in);
@@ -104,12 +81,20 @@ public class ChessBoard {
 
         System.out.println();
         System.out.println("Welcome to Chess!\n");
+
+        System.out.println("Would you like to play with coordinates or descriptive moves? Say \"coordinates\" or \"descriptive\".");
+        String descriptive = input.nextLine();
+
+        game.descriptive_moves = (descriptive.equals("descriptive")) ? true : false;
+
         System.out.println("Let's see our initial board:\n");
+
         while (true)
         {
             game.print();
             System.out.println();
-            System.out.println("next player, make your move!");
+
+            System.out.println("Next player, make your move!");
             String command_text = input.nextLine();
             try {
                 Command command = parser.parse(command_text, game);
@@ -117,36 +102,6 @@ public class ChessBoard {
             } catch(Exception e) {
                 System.out.println(e);
             }
-
-            // switch(command_tokens[0]) {
-            //     case "move":
-            //         int old_row = Integer.parseInt(command_tokens[1]);
-            //         int old_col = Integer.parseInt(command_tokens[2]);
-            //         int new_row = Integer.parseInt(command_tokens[3]);
-            //         int new_col = Integer.parseInt(command_tokens[4]);
-            //         char old_piece = game.movePiece(old_row, old_col, new_row, new_col);
-            //         old_piece = (old_piece == ' ') ? '_' : old_piece; 
-            //         game.history.push(command + " " + old_piece);
-            //         break;
-            //     case "place":
-            //         char piece = command_tokens[1].charAt(0);
-            //         int row = Integer.parseInt(command_tokens[2]);
-            //         int col = Integer.parseInt(command_tokens[3]);
-            //         old_piece = game.placePiece(row, col, piece);
-            //         old_piece = (old_piece == ' ') ? '_' : old_piece;
-            //         game.history.push(command + " " + old_piece);
-            //         break;
-            //     case "undo":
-            //         String last_move = game.history.pop();
-            //         game.undoMove(last_move);
-            //         break;
-            //     case "checkmate":
-            //         input.close();
-            //         return;
-            //     default:
-            //         System.out.println("Not a valid move");
-            //         break;
-            // }
         }
     }
 }
--- Parser.java
+++ Parser.java
@@ -10,18 +10,15 @@ public class Parser {
 
         switch(tokens[0]) {
             case "move":
-                if (tokens.length != 5) {
-                    throw new Exception("Move requires the following format move" +
-                                         " old_row old_col new_row new_col");
-                }
-                int old_row = Integer.parseInt(tokens[1]) - 1;
-                int old_col = Integer.parseInt(tokens[2]) - 1;
-                int new_row = Integer.parseInt(tokens[3]) - 1;
-                int new_col = Integer.parseInt(tokens[4]) - 1;
                 try {
-                    ChessTile old_tile = new ChessTile(old_row, old_col);
-                    ChessTile new_tile = new ChessTile(new_row, new_col);
-                    return new Move(chessBoard, old_tile, new_tile);
+                    if (chessBoard.descriptive_moves == false)
+                    {
+                        return parseCoordinateMoveCommand(tokens, chessBoard);
+                    }
+                    else
+                    {
+                        return parseDescriptiveMoveCommand(tokens, chessBoard);
+                    }
                 } catch (Exception e) {
                     throw e;
                 }               
@@ -46,6 +43,90 @@ public class Parser {
                 return new Checkmate();
             default:
                 throw new Exception("Not a vaild command");
+        }
+    }
+
+    public Command parseCoordinateMoveCommand(String[] tokens, ChessBoard chessBoard) throws Exception
+    {
+        if (tokens.length != 5) {
+            throw new Exception("Move requires the following format move" +
+                                 " old_row old_col new_row new_col");
+        }
+        int old_row = Integer.parseInt(tokens[1]) - 1;
+        int old_col = Integer.parseInt(tokens[2]) - 1;
+        int new_row = Integer.parseInt(tokens[3]) - 1;
+        int new_col = Integer.parseInt(tokens[4]) - 1;
+        try {
+            ChessTile old_tile = new ChessTile(old_row, old_col);
+            ChessTile new_tile = new ChessTile(new_row, new_col);
+            return new Move(chessBoard, old_tile, new_tile);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    public Command parseDescriptiveMoveCommand(String[] tokens, ChessBoard chessBoard) throws Exception
+    {
+        int old_row = 0;
+        int old_col = 0;
+        int new_row = 0;
+        int new_col = 0;
+
+        if (tokens.length != 4) {
+            throw new Exception("Move requires the following format move" +
+                                 " player descriptive_location descriptive_move");
+        }
+        int player = Integer.parseInt(tokens[1]);
+        String descriptive_location = tokens[2];
+        String descriptive_move = tokens[3];
+        try {
+            if (descriptive_location.length() == 2)
+            {
+                old_col = (descriptive_location.charAt(0) == 'Q') ? 4 : 5;
+                int temp_row = (int)descriptive_location.charAt(1);
+                old_row = (player == 1) ? temp_row : chessBoard.chess_board_size - temp_row;
+            }
+            else
+            {
+                if (descriptive_location.charAt(0) == 'Q')
+                {
+                    switch(descriptive_location.charAt(1))
+                    {
+                        case 'R':
+                            old_col = 1;
+                            break;
+                        case 'N':
+                            old_col = 2;
+                            break;
+                        case 'B':
+                            old_col = 3;
+                            break;
+                    }
+                }
+                else
+                {
+                    switch(descriptive_location.charAt(1))
+                    {
+                        case 'R':
+                            old_col = 8;
+                            break;
+                        case 'N':
+                            old_col = 7;
+                            break;
+                        case 'B':
+                            old_col = 6;
+                            break;
+                    }
+                }
+                int temp_row = (int)descriptive_location.charAt(2);
+                old_row = (player == 1) ? temp_row : chessBoard.chess_board_size - temp_row;
+            }
+            
+            ChessTile old_tile = new ChessTile(old_row, old_col);
+            ChessTile new_tile = new ChessTile(new_row, new_col);
+            return new Move(chessBoard, old_tile, new_tile);
+        } catch (Exception e) {
+            throw e;
         }
     }
 }
